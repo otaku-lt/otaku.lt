4e7043975cf91a7654497407bf9d6d6d6fa4bfc2
Roman Gorodeckij <holms@holms.lt>
2025-07-26 01:18:37 +0300

    chore: add type definition files for YAML, CSS modules, and SVG imports

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for your project
+// Project: Otaku.lt
+
+// Add type definitions here if needed
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
